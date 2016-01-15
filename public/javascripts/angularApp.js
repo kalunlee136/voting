1072b3ee19e7114a5ea913eb1ca5773740664448
Ka Lun Lee <kalun.lee1@gmail.com>
2016-01-15 00:15:49 +0000

    working poll

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router']);
+var app = angular.module('votingApp', ['ui.router']);
 
 app.controller('MainCtrl', ['$scope','$http','$state','$stateParams', function($scope,$http,$state,$stateParams){
 
